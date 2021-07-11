a31af31b98c3ad4d7de75fedcd3297341eca8a22
Ushieru <uzielcocolan@gmail.com>
2021-07-11 15:36:32 -0500

    move to gRPC folder

--- src/client/index.ts
+++
@@ -1,8 +0,0 @@
-import { TodoItem } from '../proto/todo_pb';
-import createTodo from './create_todo';
-import readTodos from './read_todos';
-
-(async () => {
-    await createTodo(new TodoItem().setText('Todo'));
-    await readTodos();
-})();
