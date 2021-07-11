10c7c2707fccc4a9d3206c0ecaa5113a75c85e45
Ushieru <uzielcocolan@gmail.com>
2021-07-11 16:54:21 -0500

    add ipv4 interface

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import { fastify } from './server';
 
-fastify.listen(3333, function (err, address) {
+fastify.listen(3333, '0.0.0.0', function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
